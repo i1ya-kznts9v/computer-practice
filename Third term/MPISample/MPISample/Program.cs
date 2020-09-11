08bfb3cb095868911e7b8ab5640504323faf2381
Stanislav Sartasov <Stanislav.Sartasov@homehero.co.il>
2020-09-11 11:45:30 +0300

    - rearranged structure, added third term

---
+++ Third term/MPISample/MPISample/Program.cs
@@ -0,0 +1,29 @@
+﻿using MPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MPISample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var env = new MPI.Environment(ref args))
+            {
+                Intracommunicator world = Communicator.world;
+                int rank = world.Rank;
+
+                int result = world.Reduce(rank, Operation<int>.Add, 0);
+                if(world.Rank == 0)
+                {
+                    Console.WriteLine(result);
+                }
+                
+            }
+        }
+    }
+}
