f7b4c2ae01d11171caceedc2ac346b6dcd38fa0c
Kuznetsov Ilya Alexandrovich <kuznetsov.ilya.alexandrovich@gmail.com>
2020-10-25 23:17:38 +0300

    Implemented "Future" task (v 1.0)

---
+++ Third semester/Completed homeworks/Homework 4/Future/Future.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Future.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Future.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f860b3f0-0f33-4edf-8b97-02d509a1283b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
